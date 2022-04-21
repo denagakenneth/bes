02fbb2dbd782e5e9cf04ef89b51a2844f48ddf36
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-21 23:09:01 +0800

    gfgfgfgXY

--- KDBES/views.py
+++ KDBES/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def MainPage(request):
-	return HttpResponse('<html><title>Barangay Event Schedule</title></html>')
+	return HttpResponse('<html><title>Chrono Intelligence - World News</title></html>')
 # Create your views here.
