3238c53b5800fd645ffd39503aec1695e45b3ca8
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-21 22:48:34 +0800

    commit2

--- kenlist/urls.py
+++ kenlist/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from msystem import views
+from kdbes import views
 
 urlpatterns = [
     path('', views.MainPage, name='mainpage'),
