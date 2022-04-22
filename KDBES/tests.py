881b5bb8665f80719ac104b96437ea4c2c66a53b
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-22 12:47:21 +0800

    commitirist

--- KDBES/tests.py
+++ KDBES/tests.py
@@ -26,6 +26,6 @@ class HomePageTest(TestCase):
 		self.assertIn('LNameNew', resp.content.decode())
 		self.assertTemplateUsed(resp, 'mainpage.html')
 		
-		resp = self.client.post('/', data={'MresAddress' :'MNameAddress'})
+		resp = self.client.post('/', data={'MresAddress' :'MAddressNew'})
 		self.assertIn('MAddressNew', resp.content.decode())
 		self.assertTemplateUsed(resp, 'mainpage.html')
