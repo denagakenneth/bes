a67f17358a56a062a92ccb1e5e185a415200c948
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-22 11:25:55 +0800

    commitirist

--- KDBES/views.py
+++ KDBES/views.py
@@ -5,3 +5,8 @@ def MainPage(request):
 	# if request.method == 'POST':
 	# 	return HttpResponse (request.POST['attribute'])
 	return render(request, 'mainpage.html', {'NewName': request.POST.get('attribute'),})
+
+def MainPage(request):
+	# if request.method == 'POST':
+	# 	return HttpResponse (request.POST['attribute'])
+	return render(request, 'mainpage.html', {'NewAge': request.POST.get('attribute'),})
