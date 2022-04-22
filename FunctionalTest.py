bcbabaf9515691eda303f43d1a27b28b83f8292b
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-22 12:15:16 +0800

    commitirist

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -12,10 +12,10 @@ class PageTest(unittest.TestCase):
 		self.assertIn('Barangay Event Schedule', self.browser.title)
 		headerText = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('Resident Form', headerText)
-		inpName = self.browser.find_element_by_id('studentName')
-		inpNameF = self.browser.find_element_by_id('FstudentName')
-		inpNameL = self.browser.find_element_by_id('LstudentName')
-		inpNameM = self.browser.find_element_by_id('MstudentName')
+		inpName = self.browser.find_element_by_id('resName')
+		inpNameF = self.browser.find_element_by_id('Fresname')
+		inpNameL = self.browser.find_element_by_id('LresName')
+		inpNameM = self.browser.find_element_by_id('MresName')
 		btn_button = self.browser.find_element_by_id('btn')
 		self.assertEqual(inpName.get_attribute('placeholder'),'Enter your name here.')
 		inpName.click()
@@ -33,7 +33,7 @@ class PageTest(unittest.TestCase):
 	def checking_if_in_table_list(self,row_test):
 		table = self.browser.find_element_by_id('resTable')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Scathach', [rows.text for rows in rows])
+		self.assertIn('1: Arthur', [rows.text for rows in rows])
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
