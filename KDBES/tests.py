ea00ccfc92f2093a4099d8bb9136b0db63fff468
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-23 13:31:24 +0800

    commitirist5

--- KDBES/tests.py
+++ KDBES/tests.py
@@ -37,3 +37,34 @@ class HomePageTest(TestCase):
 		resp = self.client.post('/', data={'resContact' :'ContactNew'})
 		self.assertIn('ContactNew', resp.content.decode())
 		self.assertTemplateUsed(resp, 'mainpage.html')
+
+'''class ORM(TestCase):
+
+   def test_saving_and_retrieving_items(self):
+      list_ = List()        
+      list_.save()
+      
+      first_item = Item()        
+      first_item.donatorsname = 'The first (ever) list item' 
+      first_item.list = list_ 
+      first_item.save()        
+               
+      second_item = Item()      
+      second_item.am = 'Item the second'
+      second_item.list = list_         
+      second_item.save()
+       
+       
+      saved_list = List.objects.first()          
+      self.assertEqual(saved_list, list_)
+                 
+      saved_items = Item.objects.all()
+      self.assertEqual(saved_items.count(), 2)
+       
+      first_saved_item = saved_items[0]
+      second_saved_item = saved_items[1]      	     
+      self.assertEqual(first_saved_item.fullname, 'The first (ever) list item')
+      self.assertEqual(first_saved_item.list, list_)
+      self.assertEqual(second_saved_item.ni, 'Item the second')
+      self.assertEqual(second_saved_item.list, list_)'''
+
