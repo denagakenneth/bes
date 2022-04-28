915fe0633a72560932af09fb6d1928c8cfd4e015
kenneth denaga <kennethdenaga1212@gmail.com>
2022-04-29 02:01:46 +0800

    mitmit

--- KDBES/models.py
+++ KDBES/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Resident(models.Model):
+	brgyid = models.TextField(default='')
+	FresName = models.TextField(default='')
+	MresName = models.TextField(default='')
+	LresName = models.TextField(default='')
+	resAddress = models.TextField(default='')
+	resAge= models.TextField(default='')
+	resBday = models.TextField(default='')
+	resContact = models.TextField(default='')
+	registration = models.ForeignKey(Registration, default=None, on_delete=models.CASCADE)
+
+'''class Registration(models.Model):
+	brgyID = models.TextField(default='')
+	brgy = models.TextField(default='')
+	brgyadd = models.TextField(default='')'''
