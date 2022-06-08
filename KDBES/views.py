15618cb6de9f8a6370539199471e9a537b897bfb
kenneth denaga <kennethdenaga1212@gmail.com>
2022-06-08 20:10:24 +0800

    editing views and url

--- KDBES/views.py
+++ KDBES/views.py
@@ -18,7 +18,9 @@ def view_list(request, rinfo_id):
    return render(request, 'b.html', {'rinfo': rinfo_})
    
 def add_item(request):     
-   newrinfo_ = Rinfo.objects.create(rlname=request.POST['lastname'],rfname=request.POST['firstname'],rmname=request.POST['middlename'],raddress=request.POST['rraddress'],rage=request.POST['rrage'],rcnumber=request.POST['rccnumber'],runame=request.POST['rruname'],rpass=request.POST['rrpass'])
+   newrinfo_ = Rinfo.objects.create(rlname=request.POST['lastname'],rfname=request.POST['firstname'],rmname=request.POST['middlename'],
+    raddress=request.POST['rraddress'],rage=request.POST['rrage'],rcnumber=request.POST['rccnumber'],runame=request.POST['rruname'],
+    rpass=request.POST['rrpass'])
    return redirect(f'/{newrinfo_.id}/') 
   
 
