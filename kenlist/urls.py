e79f588c8ffb68b99b7572dbd4da47a420bb56f8
kenneth denaga <kennethdenaga1212@gmail.com>
2022-06-13 21:38:02 +0800

    working on

--- kenlist/urls.py
+++ kenlist/urls.py
@@ -16,16 +16,16 @@ urlpatterns = [
     url(r'^admin_log/$', views.admin_log, name='admin_log'),
     url(r'^aaccount$', views.aaccount, name='aaccount'),
 
+    url(r'^register$', views.register, name='register'),
+    url(r'^(\d+)/next/eventview$', views.eventview, name='eventview'),
+    url(r'^(\d+)/eventstatus$', views.eventstatus, name='eventstatus'),
+    url(r'^(\d+)/eventstatus/statusupdate$', views.statusupdate, name='statusupdate'),
+
     url(r'^location$', views.view_location, name='view_location'),
     url(r'^add_location$', views.add_location, name='add_location'),
     url(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
     url(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
     url(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
-    
-    url(r'^register$', views.register, name='register'),
-    url(r'^(\d+)/next/eventview$', views.eventview, name='eventview'),
-    url(r'^(\d+)/eventstatus$', views.eventstatus, name='eventstatus'),
-    url(r'^(\d+)/eventstatus/statusupdate$', views.statusupdate, name='statusupdate'),
     
     url(r'^about$', views.about, name='about'),
     url(r'^contact$', views.contact, name='contact'),
